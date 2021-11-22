753e1ee59617ede42ab501eaf543c59cf7e9b6bb
Mitchell Sullivan <mitchellsullivan@users.noreply.github.com>
2021-11-22 09:25:14 -0600

    Update nginx-ssl-docker.sh

--- nginx-ssl-docker.sh
+++ nginx-ssl-docker.sh
@@ -2,7 +2,7 @@
 # Used to reverse proxy to docker containers, all on one machine.
 
 # install requirements for docker
-sudo apt update
+sudo apt-get update
 sudo apt-get install ca-certificates curl gnupg lsb-release -y
 
 # add official docker keys
@@ -10,21 +10,23 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | \
   sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] \
-  https://download.docker.com/linux/ubuntu$(lsb_release -cs) stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  https://download.docker.com/linux/ubuntu$(lsb_release -cs) stable" \
+  | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # install docker
-sudo apt update
+sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 sudo usermod -aG docker $USER
 newgrp docker
 
 # install docker-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L \
+  "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" \
+  -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 # install nginx
-sudo apt install nginx apache2-utils -y
+sudo apt-get install nginx apache2-utils -y
 
 # install certbot
 sudo snap install core; sudo snap refresh core
