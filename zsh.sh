1d47f134b47957bce74929cf0ef8b996cd418773
Mitchell Sullivan <mitchellsullivan@users.noreply.github.com>
2021-12-04 11:13:59 -0600

    Update and rename zsh-kubectl.sh to zsh.sh

---
+++ zsh.sh
@@ -0,0 +1,14 @@
+sudo apt update
+
+sudo apt install zsh -y
+sudo usermod --shell $(which zsh) $(whoami)
+
+cat <<-EOF >> ~/.zshrc 
+export PATH="\$PATH:/snap/bin"
+alias ls='ls --color=auto'
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+EOF
+
+
