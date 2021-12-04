3a5a9e62d836e8cdbab1bb28d713bea8f378f88f
Mitchell Sullivan <mitchellsullivan@users.noreply.github.com>
2021-12-04 11:14:12 -0600

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -3,7 +3,7 @@ sudo apt update
 sudo apt install zsh -y
 sudo usermod --shell $(which zsh) $(whoami)
 
-cat <<-EOF >> ~/.zshrc 
+cat <<EOF >> ~/.zshrc 
 export PATH="\$PATH:/snap/bin"
 alias ls='ls --color=auto'
 alias ll='ls -alF'
